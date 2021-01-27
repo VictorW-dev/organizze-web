331bef0ab9373cc789efc31baf179b1419c67d04
VictorW-dev <victorwinicius.bs@gmail.com>
2021-01-26 23:50:04 -0300

    First commit

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'> 
+        <strong> 
+            Copyright &copy; 2021
+            <a href='http://github.com/VictorW-dev' target='_blank'> VictorW-dev</a>.
+        </strong>
+    </footer>
+)
